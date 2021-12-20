98543d1cd1ce2b27a6014e23897589dc97286203
MGitLop <mcursoslapdell@gmail.com>
2021-12-20 11:05:48 -0300

    :sparkles: 01:02:58 - Navegacao ente componentes
    #

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 function Inicio(){
   return (
     <div>
-      <h1>Fundamentos de Next.js & React</h1>
-      <h2>Hoje vamos estudar este framework</h2>
+      <Link href="/estiloso">
+          Estiloso
+      </Link>
     </div>
   ) 
 }
