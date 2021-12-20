b8d927ed56959fa3644d804f134b881fa75c5a54
MGitLop <mcursoslapdell@gmail.com>
2021-12-20 07:48:36 -0300

    :sparkles: 30:25 Intregação JavaScript/JSX
    #

---
+++ src/pages/jsx.jsx
@@ -0,0 +1,31 @@
+export default function Jsx(){
+
+  const titulo = <h1>Título usando uma constante</h1>
+  const a = 3;
+  const b = 4;
+  console.log(a * b);
+
+  function subTitulo(){
+    return <h2>Subtítulo atraves de função</h2>
+  }
+
+  return(
+    <div>
+
+      {titulo}
+      {subTitulo()}
+
+      <h1>JSX é um conceito Central</h1>
+      { a * b }
+      <br />
+      {"muito legal".toUpperCase()}
+      <h2>
+        {"muito legal".toUpperCase()}
+      </h2>
+
+      <p>
+        {JSON.stringify({nome: 'João', idade: 30})}
+      </p>
+    </div>
+  )
+}
---
+++ src/pages/teste.jsx
@@ -0,0 +1,5 @@
+export default function Teste(){
+  // return "<h1>Título</h1>"; ao usar jsx não precisa aspas
+  return <h1>Título</h1>
+
+}
